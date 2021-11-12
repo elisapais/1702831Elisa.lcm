19adb80662169329ec3b8dc512fb3be55365e109
elisapais <94174780+elisapais@users.noreply.github.com>
2021-11-12 12:28:09 +0000

    subida da aplicação

---
+++ app/src/main/java/com/example/tesste_elisa_1702831/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.tesste_elisa_1702831;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
