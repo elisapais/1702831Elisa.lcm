b666adb5a8f26622f92600d13a10dc18011f53f7
elisapais <94174780+elisapais@users.noreply.github.com>
2021-11-26 12:57:04 +0000

    Inserir os botões
    cada botão para pagina a seguir

---
+++ Restours/app/src/main/java/com/example/restours/MainActivity2.java
@@ -0,0 +1,23 @@
+package com.example.restours;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    public void sendActv3(View view) {
+    }
+
+    public void sendActv(View view) {
+    Intent intent = getIntent();
+    }
+}
